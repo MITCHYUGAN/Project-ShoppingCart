957cd379f9615a048cc2fca7ed9eef789b5f820c
MITCHYUGAN <denmichigan123@gmail.com>
2025-01-03 07:38:45 +0100

    Downloaded useful packages like react-router-dom, material-ui, styled-components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+        <img src="" alt="Logo" />
+        <ul>
+            <li>HomePage</li>
+            <li>ShopPage</li>
+        </ul>
+        <img src="" alt="Cart Icon" />
+    </nav>
+  )
+}
+
+export default Navbar
