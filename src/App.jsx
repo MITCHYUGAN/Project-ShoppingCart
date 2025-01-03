fdc59ef8a5ea0541d7603baec1843d54382e277b
MITCHYUGAN <denmichigan123@gmail.com>
2025-01-03 07:43:12 +0100

    refactor: Refactored app to reflex react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import ShopPage from "./pages/ShopPage"
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
+      </Routes>
     </>
   )
 }
