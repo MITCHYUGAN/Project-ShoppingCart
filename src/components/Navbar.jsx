7eb4a2e8ae5b6cb01d9128cc68649afcb56b940b
MITCHYUGAN <denmichigan123@gmail.com>
2025-01-03 07:40:14 +0100

    Developed the Navbar component and styled it using styled-components. also added the cart icon from materialUi-icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,36 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+const StyledNav = styled.nav`
+  width: 100%;
+  height: 100px;
+  background-color: white;
+  display: flex;
+  color: black;
+  align-items: center;
+  justify-content: space-evenly;
+  position: fixed;
+  top: 0;
+`
+
+const StyledUl = styled.ul`
+  display: flex;
+  gap: 20px
+`
+
 const Navbar = () => {
   return (
-    <nav>
-        <img src="" alt="Logo" />
-        <ul>
-            <li>HomePage</li>
-            <li>ShopPage</li>
-        </ul>
-        <img src="" alt="Cart Icon" />
-    </nav>
+    <StyledNav>
+      <h1>
+        <Link style={{textDecoration: "none"}} to="/">ShopRT</Link>
+      </h1>
+        <StyledUl>
+            <Link to="/">HomePage</Link>
+            <Link to="/shop">ShopPage</Link>
+        </StyledUl>
+        <ShoppingCartIcon style={{color: "cornflowerBlue", cursor: "pointer"}} color='primary' />
+    </StyledNav>
   )
 }
 
